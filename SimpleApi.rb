e2309b27d82cc8b577688da81dfc445bf2636aa7
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-04-27 14:46:23 +0100

    V2

--- SimpleApi.rb
+++ SimpleApi.rb
@@ -1,38 +1,22 @@
-
 require 'sinatra'
 require 'json'
 $CustomerTypes = %w[Paying Free]
 
 get '/GetAllCustomerTypes' do
   content_type :json
-  return { CustomerTypes: $CustomerTypes}.to_json
+  return { CustomerTypes: $CustomerTypes }.to_json
 end
 
 ## nothing below here is used currently but for a future video
 
 post '/GetFees' do
-  data = ReturnData.new
-  data.CustomerType = JSON.parse(request.body.read)["CustomerType"]
-  data.Amount = 0
-  data.Error = ""
+  customerType = JSON.parse(request.body.read)["CustomerType"]
 
-
-  if ($CustomerTypes.include?  data.CustomerType) == false
-    data.Error = "customerType not found"
-    data.CustomerType = ""
-  end
-
-  if(  data.CustomerType == "Paying")
-    data.Amount = Time.now.sec.abs
+  if ($CustomerTypes.include? customerType) == false
+    return { Error: "customerType not found" }.to_json
   end
 
   content_type :json
-  return { CustomerTypes:data.CustomerType,
-          Amount:data.Amount,
-          Error:data.Error}.to_json
-end
-
-
-class ReturnData
-  attr_accessor :Error,:Amount,:CustomerType
+  return { CustomerTypes: customerType,
+           Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
 end
