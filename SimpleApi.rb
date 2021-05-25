f3c75fd84839721a286f2e05cc2f7dea5999168b
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-05-25 23:43:22 +0100

    V2 refresh

--- SimpleApi.rb
+++ SimpleApi.rb
@@ -8,17 +8,13 @@ get '/GetAllCustomerTypes' do
 end
 
 ## nothing below here is used currently but for a future video
-
 post '/GetFees' do
   customerType = JSON.parse(request.body.read)["CustomerType"]
 
   content_type :json
-  if ($CustomerTypes.include? customerType) == false
-    return { Error: "customerType not found" }.to_json
-  end
-
-  return { CustomerTypes: customerType,
-           Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
+  return ($CustomerTypes.include? customerType) == false ?
+           { Error: "customerType not found" }.to_json :
+           { CustomerTypes: customerType, Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
 end
 
 
