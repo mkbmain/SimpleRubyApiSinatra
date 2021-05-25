054923fdd94e197b57560315326670d2a9a1227c
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-05-25 23:56:16 +0100

    status codes

--- SimpleApi.rb
+++ SimpleApi.rb
@@ -1,20 +1,25 @@
 require 'sinatra'
 require 'json'
-$CustomerTypes = %w[Paying Free]
+@@CustomerTypes = %w[Paying Free]
 
 get '/GetAllCustomerTypes' do
   content_type :json
-  return { CustomerTypes: $CustomerTypes }.to_json
+  status 200
+  return { CustomerTypes: @@CustomerTypes  }.to_json
 end
 
 ## nothing below here is used currently but for a future video
 post '/GetFees' do
   customerType = JSON.parse(request.body.read)["CustomerType"]
 
+  if (@@CustomerTypes.include? customerType) == false
+    status 400
+    return { Error: "customerType not found" }.to_json
+  end
+
   content_type :json
-  return ($CustomerTypes.include? customerType) == false ?
-           { Error: "customerType not found" }.to_json :
-           { CustomerTypes: customerType, Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
+  status 200
+  return { CustomerTypes: customerType, Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
 end
 
 
