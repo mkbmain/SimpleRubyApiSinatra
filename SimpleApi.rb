d56dfbc0ad462339e21d5b0adbecdcfd2938928d
michael.bourke@live.co.uk <michael.bourke@live.co.uk>
2021-05-25 23:58:11 +0100

    content type

--- SimpleApi.rb
+++ SimpleApi.rb
@@ -11,13 +11,13 @@ end
 ## nothing below here is used currently but for a future video
 post '/GetFees' do
   customerType = JSON.parse(request.body.read)["CustomerType"]
+  content_type :json
 
   if (@@CustomerTypes.include? customerType) == false
     status 400
     return { Error: "customerType not found" }.to_json
   end
 
-  content_type :json
   status 200
   return { CustomerTypes: customerType, Amount: customerType == "Paying" ? Time.now.sec.abs : 0 }.to_json
 end
